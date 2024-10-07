87f6091c01e0ac637124f54c4ddd09d929600aa1
aymen <aymen2255@hotmail.fr>
2024-10-07 04:14:25 +0200

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'chat/:userId', component: ChatComponent }
 ];
 
 @NgModule({
