1f6ac9c40b0475293ffb2c1a880ce56403eaeb19
aymen <aymen2255@hotmail.fr>
2024-10-07 03:50:16 +0200

    implement home page

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  public supportClient(id: number): void {
+    this.router.navigateByUrl('/chat/' + id);
+  }
+}
